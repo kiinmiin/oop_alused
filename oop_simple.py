7e44c12600bf27a5321a87de8c8c4634b816bea5
kiinmiin <rasmo.kiin@voco.ee>
2024-01-10 09:59:26 +0200

    simple oop exercise

--- oop_simple.py
+++ oop_simple.py
@@ -1,1 +1,14 @@
 """Simple class."""
+class Student:
+    
+    def __init__(self, name):
+        """Student class construct.
+        
+        Creates student type object.
+        
+        Parameters:
+        Name (str): Name of student.
+        """
+        self.name = name
+        self.finished = False
+        
