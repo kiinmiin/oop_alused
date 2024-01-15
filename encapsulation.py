d18c6bf2f20452d028780ce6e4cfeeb1ae720921
kiinmiin <rasmo.kiin@voco.ee>
2024-01-15 13:17:08 +0200

    encapsulation exercises

---
+++ encapsulation.py
@@ -0,0 +1,26 @@
+"""Encapsulation exercise."""
+
+
+class Student:
+    """Represent student with name, id and status."""
+    def __init__(self, name, id):
+        self.__name = name
+        self.__id = id
+        self.__status = "Active"
+        
+    pass
+
+    def get_id(self):
+        return self.__id
+        
+    def set_name(self, name):
+        self.__name = name
+    
+    def get_name(self):
+        return self.__name
+        
+    def set_status(self, status):
+        self.__status = status
+    
+    def get_status(self):
+        return self.__status
