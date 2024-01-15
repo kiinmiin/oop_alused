7c21602e649010ebe33fca64b429f922e76ea9ee
kiinmiin <rasmo.kiin@voco.ee>
2024-01-15 14:49:40 +0200

    encapsulation exercises

--- encapsulation.py
+++ encapsulation.py
@@ -3,11 +3,12 @@
 
 class Student:
     """Represent student with name, id and status."""
+    
     def __init__(self, name, id):
+        """Class Student constructor."""
         self.__name = name
         self.__id = id
         self.__status = "Active"
-        
     pass
 
     def get_id(self):
@@ -15,12 +16,14 @@ class Student:
         
     def set_name(self, name):
         self.__name = name
-    
+        
     def get_name(self):
         return self.__name
         
     def set_status(self, status):
-        self.__status = status
+        allowed_statuses = ['Active', 'Expelled', 'Finished', 'Inactive']
+        if status in allowed_statuses:
+            self.__status = status
     
     def get_status(self):
         return self.__status
